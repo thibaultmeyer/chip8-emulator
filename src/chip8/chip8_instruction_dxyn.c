49687373c2d0005771083f09005da47d8e8ca91b
Thibault Meyer <meyer.thibault@gmail.com>
2020-01-10 08:29:17 +0100

    Fix assigned value never used
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/chip8/chip8_instruction_dxyn.c
+++ src/chip8/chip8_instruction_dxyn.c
@@ -1,25 +1,28 @@
 #include "chip8.h"
 
 inline void chip8_instruction_dxyn(s_chip8_cpu *cpu, uint8_t b1, uint8_t b2, uint8_t b3) {
-    uint8_t x      = 0;
-    uint8_t y      = 0;
-    uint8_t k      = 0;
-    uint8_t j      = 0;
-    uint8_t coding = 0;
-    uint8_t shift  = 0;
+    uint8_t x;
+    uint8_t y;
+    uint8_t yline;
+    uint8_t xline;
+    uint8_t coding;
+    uint8_t shift;
 
     cpu->reg[0xF] = 0x00;
-    for (k = 0; k < b1; ++k) {
-        coding = cpu->memory[cpu->i + k];
-        y      = (cpu->reg[b2] + k) % CHIP8_VIDEO_DIM_HEIGHT;
-        for (j = 0, shift = 7; j < 8; ++j, --shift) {
-            x = (cpu->reg[b3] + j) % CHIP8_VIDEO_DIM_WIDTH;
+    for (yline = 0; yline < b1; ++yline) {
+        coding = cpu->memory[cpu->i + yline];
+        y      = (cpu->reg[b2] + yline) % CHIP8_VIDEO_DIM_HEIGHT;
+
+        for (xline = 0, shift = 7; xline < 8; ++xline, --shift) {
+            x = (cpu->reg[b3] + xline) % CHIP8_VIDEO_DIM_WIDTH;
+
             if (((coding) & (0x1 << shift)) != 0) {
                 if (cpu->video[y][x] == 1) {
                     cpu->video[y][x] = 0;
                     cpu->reg[0xF]    = 1;
-                } else
+                } else {
                     cpu->video[y][x] = 1;
+                }
             }
         }
     }
