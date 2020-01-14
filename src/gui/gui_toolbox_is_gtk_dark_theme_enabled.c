5ff25319449998f796e6822925f747de1ccc6c2d
Thibault Meyer <meyer.thibault@gmail.com>
2020-01-14 19:35:10 +0100

    Free settings & value before returning
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/gui/gui_toolbox_is_gtk_dark_theme_enabled.c
+++ src/gui/gui_toolbox_is_gtk_dark_theme_enabled.c
@@ -3,7 +3,13 @@
 gboolean gui_toolbox_is_gtk_dark_theme_enabled(void) {
     GtkSettings *settings = gtk_settings_get_default();
     gchar       *value;
+    gboolean    res;
 
     g_object_get(settings, "gtk-theme-name", &value, NULL);
-    return g_strrstr(value, "dark") != NULL;
+    res = g_strrstr(value, "dark") != NULL;
+
+    g_object_unref(settings);
+    g_free(value);
+
+    return res;
 }
