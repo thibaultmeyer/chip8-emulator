7d4061156b06065983d1b23be916c70f644a4ef6
Thibault Meyer <meyer.thibault@gmail.com>
2020-03-15 10:03:52 +0100

    Check NULL on CPU initialisation
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/gui/gui_main_callback_header_bar_load_rom.c
+++ src/gui/gui_main_callback_header_bar_load_rom.c
@@ -30,80 +30,82 @@ void gui_main_callback_header_bar_load_rom(GtkApplication *app, gpointer user_da
         gtk_widget_destroy(dialog);
 
         // Initialize a new chip8 CPU
-        s_chip8_cpu *cpu = (s_chip8_cpu *) malloc(sizeof(s_chip8_cpu) + 1);
-        chip8_initialize(cpu, &gui_chip8_callback_opcode_error);
+        s_chip8_cpu *cpu = (s_chip8_cpu *) malloc(sizeof(s_chip8_cpu));
+        if (cpu != NULL) {
+            chip8_initialize(cpu, &gui_chip8_callback_opcode_error);
 
-        // Load ROM
-        e_loadrom_status status = chip8_load_rom(cpu, filename);
-        GtkWidget        *error_msg_dialog;
-        switch (status) {
-            case LOAD_ROM_STATUS_ERROR_NOT_A_VALID_FILE: {
-                error_msg_dialog = gtk_message_dialog_new(GTK_WINDOW(gl_gui_components.gtk_window),
-                                                          GTK_DIALOG_DESTROY_WITH_PARENT,
-                                                          GTK_MESSAGE_ERROR,
-                                                          GTK_BUTTONS_CLOSE,
-                                                          "Not a valid ROM file");
-                gtk_dialog_run(GTK_DIALOG(error_msg_dialog));
-                gtk_widget_destroy(error_msg_dialog);
-                free(cpu);
-                break;
-            }
-            case LOAD_ROM_STATUS_ERROR_FILE_TOO_BIG: {
-                error_msg_dialog = gtk_message_dialog_new(GTK_WINDOW(gl_gui_components.gtk_window),
-                                                          GTK_DIALOG_DESTROY_WITH_PARENT,
-                                                          GTK_MESSAGE_ERROR,
-                                                          GTK_BUTTONS_CLOSE,
-                                                          "ROM file is too big");
-                gtk_dialog_run(GTK_DIALOG(error_msg_dialog));
-                gtk_widget_destroy(error_msg_dialog);
-                free(cpu);
-                break;
-            }
-            case LOAD_ROM_STATUS_ERROR_CANT_OPEN_FILE: {
-                error_msg_dialog = gtk_message_dialog_new(GTK_WINDOW(gl_gui_components.gtk_window),
-                                                          GTK_DIALOG_DESTROY_WITH_PARENT,
-                                                          GTK_MESSAGE_ERROR,
-                                                          GTK_BUTTONS_CLOSE,
-                                                          "Can't open ROM file");
-                gtk_dialog_run(GTK_DIALOG(error_msg_dialog));
-                gtk_widget_destroy(error_msg_dialog);
-                free(cpu);
-                break;
-            }
-            case LOAD_ROM_STATUS_ERROR_READ_ERROR: {
-                error_msg_dialog = gtk_message_dialog_new(GTK_WINDOW(gl_gui_components.gtk_window),
-                                                          GTK_DIALOG_DESTROY_WITH_PARENT,
-                                                          GTK_MESSAGE_ERROR,
-                                                          GTK_BUTTONS_CLOSE,
-                                                          "Cannot read the whole ROM file");
-                gtk_dialog_run(GTK_DIALOG(error_msg_dialog));
-                gtk_widget_destroy(error_msg_dialog);
-                free(cpu);
-                break;
-            }
-            default:
-                // Stop emulation
-                gui_emulation_timer_stop();
-
-                // Use newly created CPU instance
-                if (gl_gui_components.chip8_cpu) {
-                    chip8_free_cpu(gl_gui_components.chip8_cpu);
+            // Load ROM
+            GtkWidget        *error_msg_dialog;
+            e_loadrom_status status = chip8_load_rom(cpu, filename);
+            switch (status) {
+                case LOAD_ROM_STATUS_ERROR_NOT_A_VALID_FILE: {
+                    error_msg_dialog = gtk_message_dialog_new(GTK_WINDOW(gl_gui_components.gtk_window),
+                                                              GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                              GTK_MESSAGE_ERROR,
+                                                              GTK_BUTTONS_CLOSE,
+                                                              "Not a valid ROM file");
+                    gtk_dialog_run(GTK_DIALOG(error_msg_dialog));
+                    gtk_widget_destroy(error_msg_dialog);
+                    free(cpu);
+                    break;
                 }
-                gl_gui_components.chip8_cpu = cpu;
-
-                // Update UI
-                gchar *basename = g_filename_display_basename(filename);
-                gtk_header_bar_set_subtitle(GTK_HEADER_BAR(gl_gui_components.gtk_header_bar), basename);
-
-                // Keep ROM basename for load/save state
-                if (gl_gui_components.current_rom_name != NULL) {
-                    g_free(gl_gui_components.current_rom_name);
+                case LOAD_ROM_STATUS_ERROR_FILE_TOO_BIG: {
+                    error_msg_dialog = gtk_message_dialog_new(GTK_WINDOW(gl_gui_components.gtk_window),
+                                                              GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                              GTK_MESSAGE_ERROR,
+                                                              GTK_BUTTONS_CLOSE,
+                                                              "ROM file is too big");
+                    gtk_dialog_run(GTK_DIALOG(error_msg_dialog));
+                    gtk_widget_destroy(error_msg_dialog);
+                    free(cpu);
+                    break;
                 }
-                gl_gui_components.current_rom_name = basename;
+                case LOAD_ROM_STATUS_ERROR_CANT_OPEN_FILE: {
+                    error_msg_dialog = gtk_message_dialog_new(GTK_WINDOW(gl_gui_components.gtk_window),
+                                                              GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                              GTK_MESSAGE_ERROR,
+                                                              GTK_BUTTONS_CLOSE,
+                                                              "Can't open ROM file");
+                    gtk_dialog_run(GTK_DIALOG(error_msg_dialog));
+                    gtk_widget_destroy(error_msg_dialog);
+                    free(cpu);
+                    break;
+                }
+                case LOAD_ROM_STATUS_ERROR_READ_ERROR: {
+                    error_msg_dialog = gtk_message_dialog_new(GTK_WINDOW(gl_gui_components.gtk_window),
+                                                              GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                              GTK_MESSAGE_ERROR,
+                                                              GTK_BUTTONS_CLOSE,
+                                                              "Cannot read the whole ROM file");
+                    gtk_dialog_run(GTK_DIALOG(error_msg_dialog));
+                    gtk_widget_destroy(error_msg_dialog);
+                    free(cpu);
+                    break;
+                }
+                default:
+                    // Stop emulation
+                    gui_emulation_timer_stop();
 
-                // Start emulation
-                gui_emulation_timer_start();
-                break;
+                    // Use newly created CPU instance
+                    if (gl_gui_components.chip8_cpu) {
+                        chip8_free_cpu(gl_gui_components.chip8_cpu);
+                    }
+                    gl_gui_components.chip8_cpu = cpu;
+
+                    // Update UI
+                    gchar *basename = g_filename_display_basename(filename);
+                    gtk_header_bar_set_subtitle(GTK_HEADER_BAR(gl_gui_components.gtk_header_bar), basename);
+
+                    // Keep ROM basename for load/save state
+                    if (gl_gui_components.current_rom_name != NULL) {
+                        g_free(gl_gui_components.current_rom_name);
+                    }
+                    gl_gui_components.current_rom_name = basename;
+
+                    // Start emulation
+                    gui_emulation_timer_start();
+                    break;
+            }
         }
 
         // Free memory
