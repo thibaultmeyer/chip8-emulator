3f258558c7c2753a8d5c2250c38aa1e323d2452c
Thibault Meyer <meyer.thibault@gmail.com>
2020-03-15 12:28:07 +0100

    Sound buffer is now initialized only once
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/sound/sound_initialize.c
+++ src/sound/sound_initialize.c
@@ -1,13 +1,41 @@
+#include <math.h>
+#include <stdlib.h>
 #include <string.h>
 #include "sound.h"
 
 void sound_initialize(void) {
-    memset(&gl_sound_components, 0, sizeof(s_sound_components));
-    const char *defname = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
+    const char *defname       = alcGetString(NULL, ALC_DEFAULT_DEVICE_SPECIFIER);
+    float      freq           = 400.f;
+    int        seconds        = 1;
+    unsigned   sample_rate    = 44100;
+    double     my_pi          = 3.14159;
+    size_t     buf_size       = (seconds * sample_rate);
+    size_t     duration_bound = (seconds * sample_rate) / 60;
 
+    // Open device and initialize context
+    memset(&gl_sound_components, 0, sizeof(s_sound_components));
     gl_sound_components.openal_output_device  = alcOpenDevice(defname);
     gl_sound_components.openal_output_context = alcCreateContext(gl_sound_components.openal_output_device, NULL);
 
     alcMakeContextCurrent(gl_sound_components.openal_output_context);
     alGenBuffers(1, &gl_sound_components.internal_buffer);
+
+    // Allocate PCM audio buffer
+    short *buzzer_buffer = malloc(sizeof(short) * buf_size);
+    if (buzzer_buffer != NULL) {
+
+        for (int idx = 0; idx < buf_size; ++idx) {
+            if (idx < duration_bound) {
+                buzzer_buffer[idx] = (short) (32760 * sin((2.f * my_pi * freq) / sample_rate * idx));
+            } else {
+                buzzer_buffer[idx] = 0;
+            }
+        }
+
+        // Send buffer to OpenAL
+        alBufferData(gl_sound_components.internal_buffer, AL_FORMAT_MONO16, buzzer_buffer, buf_size, sample_rate);
+
+        // Free allocated variable
+        free(buzzer_buffer);
+    }
 }
--- src/sound/sound_play_buzzer.c
+++ src/sound/sound_play_buzzer.c
@@ -1,29 +1,7 @@
-#include <stdlib.h>
-#include <math.h>
 #include "sound.h"
 
 void sound_play_buzzer(void) {
-    float    freq        = 400.f;
-    int      seconds     = 1;
-    unsigned sample_rate = 44100;
-    double   my_pi       = 3.14159;
-    size_t   buf_size    = (seconds * sample_rate) / 2;
-
-    // Allocate PCM audio buffer
-    short *samples = malloc(sizeof(short) * buf_size);
-
-    int i = 0;
-    for (; i < buf_size; ++i) {
-        samples[i] = (short) (32760 * sin((2.f * my_pi * freq) / sample_rate * i));
-    }
-
-    // Send buffer to OpenAL
-    alBufferData(gl_sound_components.internal_buffer, AL_FORMAT_MONO16, samples, buf_size, sample_rate);
-
-    // Free allocated variable
-    free(samples);
-
-    // Set-up sound source and play buffer
+    // Set sound source and play buffer
     alGenSources(1, &gl_sound_components.streaming_source[0]);
     alSourcei(gl_sound_components.streaming_source[0], AL_BUFFER, gl_sound_components.internal_buffer);
     alSourcePlay(gl_sound_components.streaming_source[0]);
