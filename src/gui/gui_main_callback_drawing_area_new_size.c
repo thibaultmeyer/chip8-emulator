d140affea79cd6a731a8893845c050efc8cb845c
Thibault Meyer <meyer.thibault@gmail.com>
2020-09-05 10:30:20 +0200

    Fix pixels can be draw outside the drawing area
    Signed-off-by: Thibault Meyer <meyer.thibault@gmail.com>

--- src/gui/gui_main_callback_drawing_area_new_size.c
+++ src/gui/gui_main_callback_drawing_area_new_size.c
@@ -1,7 +1,13 @@
 #include "gui.h"
 
 void gui_main_callback_drawing_area_new_size(GtkWidget *widget, GtkAllocation *allocation, void *data) {
-    gl_gui_components.chip8_screen_height      = allocation->height;
-    gl_gui_components.chip8_screen_width       = allocation->width;
-    gl_gui_components.chip8_screen_pixel_ratio = allocation->width / 64;
+    gl_gui_components.chip8_screen_height = allocation->height;
+    gl_gui_components.chip8_screen_width  = allocation->width;
+
+    uint32_t new_pixel_ratio = allocation->width / CHIP8_VIDEO_DIM_WIDTH;
+    if (new_pixel_ratio * CHIP8_VIDEO_DIM_HEIGHT <= allocation->height) {
+        gl_gui_components.chip8_screen_pixel_ratio = new_pixel_ratio;
+    } else {
+        gl_gui_components.chip8_screen_pixel_ratio = allocation->height / CHIP8_VIDEO_DIM_HEIGHT;
+    }
 }
